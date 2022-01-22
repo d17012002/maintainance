93f3272296e919546b36f21667bcff756f7ab2ea
Anurag Kumar <2002anuragksingh@gmail.com>
2022-01-22 11:23:59 +0530

    Maintainanace

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.get("/", (req,res)=>{
+    res.send()
+})
+
+
+app.listen(process.env.PORT || 4000, function () {
+    let PORT;
+    if (process.env.PORT === undefined) {
+        PORT = 4000;
+    } else {
+        PORT = process.env.PORT;
+    }
+
+    console.log("server running on port " + PORT);
+});
